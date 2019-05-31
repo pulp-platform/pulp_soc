d4fbc35bf2a0a965cd489be446042c966cdf95b8
Manuel Eggimann <meggimann@iis.ee.ethz.ch>
2019-05-31 09:55:18 +0200

    Replace fpga specific bootrom with generic wrapper

--- rtl/pulp_soc/boot_rom.sv
+++ rtl/pulp_soc/boot_rom.sv
@@ -35,36 +35,18 @@ module boot_rom #(
         );
 
         assign mem_slave.add[31:ROM_ADDR_WIDTH] = '0;
-   
-    `else
 
-        xpm_memory_sprom # (
-          .MEMORY_SIZE        ( 2**(ROM_ADDR_WIDTH-2) ),
-          .MEMORY_PRIMITIVE   ( "block"               ),
-          .MEMORY_INIT_FILE   ( "none"                ),
-          .MEMORY_INIT_PARAM  ( ""                    ),
-          .USE_MEM_INIT       ( 1                     ),
-          .WAKEUP_TIME        ( "disable_sleep"       ),
-          .MESSAGE_CONTROL    ( 0                     ),
-          .ECC_MODE           ( "no_ecc"              ),
-          .AUTO_SLEEP_TIME    ( 0                     ),
-          .READ_DATA_WIDTH_A  ( 32                    ),
-          .ADDR_WIDTH_A       ( ROM_ADDR_WIDTH-2      ),
-          .READ_RESET_VALUE_A ( "0"                   ),
-          .READ_LATENCY_A     ( 1                     )
-        ) rom_mem_i (
-          .sleep          ( 1'b0                              ),
-          .clka           ( clk_i                             ),
-          .rsta           ( 1'b0                              ),
-          .ena            ( ~mem_slave.csn                    ),
-          .regcea         ( 1'b1                              ),
-          .addra          ( mem_slave.add[ROM_ADDR_WIDTH-1:2] ),
-          .injectsbiterra ( 1'b0                              ),
-          .injectdbiterra ( 1'b0                              ),
-          .douta          ( mem_slave.rdata                   ),
-          .sbiterra       (                                   ),
-          .dbiterra       (                                   )
-        );
+    `else // !`ifndef PULP_FPGA_EMUL
+
+    fpga_bootrom #(
+                   .ADDR_WIDTH(ROM_ADDR_WIDTH-2),
+                   .DATA_WIDTH(32)
+                   ) rom_mem_i (
+                            .CLK(clk_i),
+                            .CEN(mem_slave.csn),
+                            .A(mem_slave.add[ROM_ADDR_WIDTH-1:2]),
+                            .Q(mem_slave.rdata)
+                            );
 
     `endif
 
