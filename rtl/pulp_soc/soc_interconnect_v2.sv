2ba0446f3fe379d4ac62bb80d2fc418d241fed14
Manuel Eggimann <meggimann@iis.ee.ethz.ch>
2020-11-04 17:58:00 +0100

    Cut combinatorial loop between APB converter and L2 demux

--- rtl/pulp_soc/soc_interconnect_v2.sv
+++ rtl/pulp_soc/soc_interconnect_v2.sv
@@ -244,8 +244,8 @@ module soc_interconnect
                                                     //outstanding transactiions anyways
                                                     MaxSlvTrans: 4,       //Allow up to 4 in-flight transactions
                                                     //per slave port
-                                                    FallThrough: 0,       //Use the reccomended default config
-                                                    LatencyMode: axi_pkg::CUT_ALL_AX,
+                                                    FallThrough: 1,       //Use the reccomended default config
+                                                    LatencyMode: axi_pkg::CUT_MST_PORTS,
                                                     AxiIdWidthSlvPorts: AXI_MASTER_ID_WIDTH,
                                                     AxiIdUsedSlvPorts: AXI_MASTER_ID_WIDTH,
                                                     AxiAddrWidth: BUS_ADDR_WIDTH,
