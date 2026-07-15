1a5787d4cae386dc1e06beb743d71d2bfa138fb7
Simone Manoni <s.manoni@unibo.it>
2026-07-15 17:19:02 -0400

    lint: fix W123 in udma_subsystem.sv

--- rtl/udma_subsystem/udma_subsystem.sv
+++ rtl/udma_subsystem/udma_subsystem.sv
@@ -631,6 +631,11 @@ module udma_subsystem
     assign s_events[4*PER_ID_FILTER+2]  = 1'b0;
     assign s_events[4*PER_ID_FILTER+3]  = 1'b0;
 
+    // Tie off the event slots of the peripheral IDs that are not instantiated
+    for (genvar i = N_PERIPHS; i < N_PERIPH_MAX; i++) begin : gen_unused_periph_events
+      assign s_events[4*i +: 4] = '0;
+    end
+
     assign s_rx_ext_destination[CH_ID_EXT_RX_FILTER] = 'h0;
     assign s_rx_ext_stream[CH_ID_EXT_RX_FILTER]      = 'h0;
     assign s_rx_ext_stream_id[CH_ID_EXT_RX_FILTER]   = 'h0;
